4da07f7c5af083fa13ea60f577b994b4c59077fb
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2017-06-14 18:42:50 +0530

    Fixing failed tests.

--- chapter-11/springboot-mvc-rest-demo/src/main/java/com/apress/demo/controllers/PostController.java
+++ chapter-11/springboot-mvc-rest-demo/src/main/java/com/apress/demo/controllers/PostController.java
@@ -1,28 +1,5 @@
 package com.apress.demo.controllers;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.util.List;
-
-import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.apress.demo.entities.Comment;
 import com.apress.demo.entities.Post;
 import com.apress.demo.exceptions.PostDeletionException;
@@ -30,6 +7,18 @@ import com.apress.demo.exceptions.ResourceNotFoundException;
 import com.apress.demo.model.ErrorDetails;
 import com.apress.demo.repositories.CommentRepository;
 import com.apress.demo.repositories.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.List;
 
 /**
  * @author Siva
